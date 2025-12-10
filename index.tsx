2c92b18899afdf303814d1fcf442549c6ab25ccd
Storm00212 <paulmuyalikhams@gmail.com>
2025-12-10 23:02:24 +0300

    fixes

--- index.tsx
+++ index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import './src/index.css';
 import App from './App';
 
 const rootElement = document.getElementById('root');
