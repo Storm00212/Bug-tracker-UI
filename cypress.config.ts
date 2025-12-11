291fe421350e7f5b0c4ab3754a1295df6dd17117
Storm00212 <paulmuyalikhams@gmail.com>
2025-12-11 11:26:59 +0300

    modified config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,25 +1,10 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
-    viewportWidth: 1280,
-    viewportHeight: 720,
-    video: false,
-    screenshotOnRunFailure: true,
-    defaultCommandTimeout: 10000,
-    requestTimeout: 15000,
-    responseTimeout: 15000,
-    supportFile: 'cypress/support/e2e.ts',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "http://localhost:5173"
   },
-  component: {
-    devServer: {
-      framework: 'react',
-      bundler: 'vite',
-    },
-    supportFile: false,
-  },
-})
+});
