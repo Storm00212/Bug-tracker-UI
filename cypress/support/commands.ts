8f9905a0e2a11cebd50a5231ed37e02e14a8aeea
Storm00212 <paulmuyalikhams@gmail.com>
2025-12-11 12:53:57 +0300

    removed extra comments

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,12 +1,4 @@
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
+
 
 /// <reference types="cypress" />
 
